3b40e8c8f78410fcfe9a51a214e14b0cebd7a3aa
0xba1 <dev.boluwatife@gmail.com>
2022-04-10 14:43:52 +0100

    priority_queue

--- ch1/priority_queue.py
+++ ch1/priority_queue.py
@@ -1,15 +1,22 @@
 import heapq
 
+# A priority queue will pop an element of the highest priority, 
+# if of equal priority, based on its index.
 class PriorityQueue:
     def __init__(self):
         self._queue = []
         self._index = 0
 
     def push(self, item, priority):
+        # The second argument is a tuple of which the heap will order 
+        # based on the ordering of its element in ascending order, i.e,
+        # first based on priority, then based on index
         heapq.heappush(self._queue, (-priority, self._index, item))
         self._index += 1
 
     def pop(self):
+        # This pops the first member of the queue(one with highest priority) 
+        # then returns the last element of that tuple
         return heapq.heappop(self._queue)[-1]
 
 
