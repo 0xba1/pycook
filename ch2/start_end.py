be5ffdf43580e2e6a6825b6fb5881d4fb0d8b62c
0xba1 <dev.boluwatife@gmail.com>
2022-05-24 06:30:39 +0100

    start_end.py

---
+++ ch2/start_end.py
@@ -0,0 +1,11 @@
+# Matching Text at the Start or End of a String
+
+def main():
+    url = 'https://blog.0xba1.xyz'
+
+    print(url.startswith('https')) # True
+    print(url.endswith('xyz')) # True
+
+
+if __name__ == "__main__":
+    main()
